18de4751396bb1126dcb24744b50544bf626bac7
Don Lucas F. <donlucasf@gmail.com>
2014-09-22 12:01:00 -0300

    A Small Project

---
+++ A Small Project/A Small Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(A_Small_Project.Startup))]
+namespace A_Small_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
